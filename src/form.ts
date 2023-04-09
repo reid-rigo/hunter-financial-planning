2a2e667d9688dc0aa1e7407fd3233d431180d56c
Reid Lynch <reid.lynch@gmail.com>
2023-04-09 11:18:56 -0400

    format inputs

--- src/form.ts
+++ src/form.ts
@@ -4,10 +4,12 @@ function onLifePlanSubmit(form: HTMLFormElement, callback: (lifePlan: LifePlan) 
   form?.addEventListener("submit", event => {
     event.preventDefault()
 
-    const initialAmount = inputToNumber(form, "initialAmount")
-    const monthlyContribution = inputToNumber(form, "monthlyContribution")
-    const years = inputToNumber(form, "years")
-    const growthRate = inputToNumber(form, "growthRate")
+    const initialAmount = numberFromInputName(form, "initialAmount")
+    const monthlyContribution = numberFromInputName(form, "monthlyContribution")
+    const years = numberFromInputName(form, "years")
+    const growthRate = numberFromInputName(form, "growthRate")
+
+    console.log(initialAmount, monthlyContribution, years, growthRate)
 
     if (initialAmount && monthlyContribution && years && growthRate) {
       const lifePlan = {
@@ -24,10 +26,55 @@ function nextYear(): number {
   return new Date().getFullYear() + 1
 }
 
-function inputToNumber(form: HTMLFormElement, inputName: string): number | null {
+function numberFromInputName(form: HTMLFormElement, inputName: string): number | null {
   const input = form.querySelector<HTMLInputElement>(`input[name="${inputName}"]`)
-  const value = input?.value
-  return value ? parseInt(value) : null
+  return input ? numberFromInput(input) : null
 }
 
-export { onLifePlanSubmit }
+function numberFromInput(input: HTMLInputElement): number | null {
+  return Number(input.value.replace(/[^0-9.,-]+/g,""))
+}
+
+function formatCurrencyInput(input: HTMLInputElement): void {
+  const focus = () => {
+    const number = numberFromInput(input)
+    if (number && number > 0) {
+      input.value = number.toString()
+    }
+  }
+
+  const blur = () => {
+    const number = numberFromInput(input)
+    if (number) {
+      input.value = new Intl.NumberFormat("en", { style: "currency", currency: "USD" }).format(number)
+    }
+  }
+
+  input.addEventListener("focus", focus)
+  input.addEventListener("blur", blur)
+
+  blur()
+}
+
+function formatPercentageInput(input: HTMLInputElement): void {
+  const focus = () => {
+    const number = numberFromInput(input)
+    if (number && number > 0) {
+      input.value = number.toString()
+    }
+  }
+
+  const blur = () => {
+    const number = numberFromInput(input)
+    if (number) {
+      input.value = `${number}%`
+    }
+  }
+
+  input.addEventListener("focus", focus)
+  input.addEventListener("blur", blur)
+
+  blur()
+}
+
+export { onLifePlanSubmit, formatCurrencyInput, formatPercentageInput }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import { calculateLifePlan } from "./calculator"
 import { chartLifePlan, Chart } from "./chart"
-import { onLifePlanSubmit } from "./form"
+import { onLifePlanSubmit, formatCurrencyInput, formatPercentageInput } from "./form"
 
-const form = <HTMLFormElement>document.querySelector("form")
+const form = document.querySelector<HTMLFormElement>("form")
 const canvas = <HTMLCanvasElement>document.querySelector("canvas#chart")
 let currentChart: Chart
 
-onLifePlanSubmit(form, lifePlan => {
-  const calculatedLifePlan = calculateLifePlan(lifePlan)
-  currentChart?.destroy()
-  currentChart = chartLifePlan(canvas, calculatedLifePlan)
-})
+if (form) {
+  form.querySelectorAll<HTMLInputElement>("input[data-currency]").forEach(input => formatCurrencyInput(input))
+  form.querySelectorAll<HTMLInputElement>("input[data-percentage]").forEach(input => formatPercentageInput(input))
+
+  onLifePlanSubmit(form, lifePlan => {
+    const calculatedLifePlan = calculateLifePlan(lifePlan)
+    currentChart?.destroy()
+    currentChart = chartLifePlan(canvas, calculatedLifePlan)
+})}
